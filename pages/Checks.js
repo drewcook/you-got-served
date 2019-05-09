60788f27e50a978bdcf1ff9b94bf78230747b938
Drew Cook <drewcook29@gmail.com>
2019-05-09 13:29:30 -0600

    Checks query

--- pages/Checks.js
+++ pages/Checks.js
@@ -1,8 +1,55 @@
 import Layout from "../client/components/Layout";
+import { GET_CHECKS } from "../queries";
+import Link from "next/link";
+import { Query } from "react-apollo";
 
 const Checks = props => (
 	<Layout title="Checks">
-		Checks page
+		<h2>Checks</h2>
+		<hr/>
+		<Query query={GET_CHECKS}>
+			{({data: {getChecks}, loading, error}) => {
+				if (loading) return <div>Loading...</div>;
+				if (error) return <div className="text-danger">{error.message}</div>;
+				return getChecks.length ?
+					<div className="row">
+						{getChecks.map(check => (
+							<div className="col-xs-12 col-sm-6 col-lg-4" key={check.id}>
+								<Link href="">
+									<a>
+										<div className="card check-wrapper border-success">
+											Check
+										</div>
+									</a>
+								</Link>
+							</div>
+						))}
+					</div> :
+					<p className="text-center">There are currently no checks.</p>;
+			}}
+		</Query>
+		<style jsx>{`
+			hr {
+				border-color: #fff;
+			}
+			.check-wrapper {
+				border-width: 2px;
+				border-radius: 5px;
+				min-height: 120px;
+				margin: 20px;
+				justify-content: center;
+				align-items: center;
+				padding: 15px;
+				text-align: center;
+			}
+			a {
+				color: #fff;
+				text-decoration: none;
+			}
+			a:hover div {
+				opacity: 0.8;
+			}
+		`}</style>
 	</Layout>
 );
 
